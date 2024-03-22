b9887ea6c37d749f8ea60bf572783df96a3a4162
Alex Verdugo <alex.verdugo@akzio.cl>
2024-03-22 10:32:03 -0300

    Primer Commit subiendo la base del proyecto

---
+++ src/libraries/akziodocumentos/AkziodocumentosLibrary.ts
@@ -0,0 +1,5 @@
+export class AkziodocumentosLibrary {
+  public name(): string {
+    return 'AkziodocumentosLibrary';
+  }
+}
---
+++ src/libraries/akziodocumentos/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAkziodocumentosLibraryStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AkziodocumentosLibraryStrings' {
+  const strings: IAkziodocumentosLibraryStrings;
+  export = strings;
+}
